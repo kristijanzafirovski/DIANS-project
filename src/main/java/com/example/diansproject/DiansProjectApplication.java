fc87f15ffd8bc682f80c38ee973560b4347a83b3
Kristijan <kristijanzafirovski26@gmail.com>
2025-02-13 23:39:46 +0100

    Initial commit

---
+++ src/main/java/com/example/diansproject/DiansProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.diansproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiansProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiansProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/diansproject/DiansProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diansproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiansProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
